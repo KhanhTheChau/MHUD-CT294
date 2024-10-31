5483502068ecc668441abdfa6bb947b1d1cf00b5
14224384 <khanhchau5678@gmail.com>
2024-10-31 23:22:29 +0700

    fix chart

---
+++ train/train.py
@@ -0,0 +1,13 @@
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# # Vẽ confusion matrix
+# cm = confusion_matrix(y_test, y_pred)
+
+# plt.figure(figsize=(8, 6))
+# sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False, 
+#             xticklabels=["Class 0", "Class 1"], 
+#             yticklabels=["Class 0", "Class 1"])
+# plt.xlabel("Predicted Labels")
+# plt.ylabel("True Labels")
+# plt.title("Confusion Matrix")
+# plt.show()
